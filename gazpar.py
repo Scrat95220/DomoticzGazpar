caab08f03858096a6b65040b6abb79eedee0628a
cth35 <51801882+cth35@users.noreply.github.com>
2024-10-28 23:31:05 +0100

    Update gazpar.py
    Using new login process thanks to : https://github.com/vingerha/gazpar_2_mqtt/blob/main/app/gazpar.py

--- gazpar.py
+++ gazpar.py
@@ -30,6 +30,7 @@ import logging
 import json
 from dateutil.relativedelta import relativedelta
 from urllib.parse import urlencode
+import http.cookiejar
 
 LOGIN_BASE_URI = 'https://login.monespace.grdf.fr/sofit-account-api/api/v1/auth'
 API_BASE_URI = 'https://monespace.grdf.fr/'
@@ -86,48 +87,62 @@ def login():
     """Logs the user into the GRDF API.
     """
     session = requests.Session()
+    SESSION_TOKEN_URL = "https://connexion.grdf.fr/api/v1/authn"
+    SESSION_TOKEN_PAYLOAD = """{{
+                "username": "{0}",
+                "password": "{1}",
+                "options": {{
+                    "multiOptionalFactorEnroll": "false",
+                    "warnBeforePasswordExpired": "false"
+                }}
+            }}"""
+    AUTH_TOKEN_URL = "https://connexion.grdf.fr/login/sessionCookieRedirect"
+    AUTH_TOKEN_PARAMS = """{{
+                "checkAccountSetupComplete": "true",
+                "token": "{0}",
+                "redirectUrl": "https://monespace.grdf.fr"
+            }}"""
+    # self.session = Session()
+    session.headers.update({"domain": "grdf.fr"})
+    session.headers.update({"Content-Type": "application/json"})
+    session.headers.update({"X-Requested-With": "XMLHttpRequest"})
 
-    payload = {
-               'email': userName,
-                'password': password,
-                'goto':'https://sofa-connexion.grdf.fr:443/openam/oauth2/externeGrdf/authorize?response_type=code%26scope=openid%20profile%20email%20infotravaux%20%2Fv1%2Faccreditation%20%2Fv1%2Faccreditations%20%2Fdigiconso%2Fv1%20%2Fdigiconso%2Fv1%2Fconsommations%20new_meg%20%2FDemande.read%20%2FDemande.write%26client_id=prod_espaceclient%26state=0%26redirect_uri=https%3A%2F%2Fmonespace.grdf.fr%2F_codexch%26nonce=7cV89oGyWnw28DYdI-702Gjy9f5XdIJ_4dKE_hbsvag%26by_pass_okta=1%26capp=meg', 
-                'capp':'meg'
-               }
-    headers = {
-                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-                'domain': 'grdf.fr',
-                'Referer': 'https://login.monespace.grdf.fr/mire/connexion?goto=https:%2F%2Fsofa-connexion.grdf.fr:443%2Fopenam%2Foauth2%2FexterneGrdf%2Fauthorize%3Fresponse_type%3Dcode%26scope%3Dopenid%2520profile%2520email%2520infotravaux%2520%252Fv1%252Faccreditation%2520%252Fv1%252Faccreditations%2520%252Fdigiconso%252Fv1%2520%252Fdigiconso%252Fv1%252Fconsommations%2520new_meg%2520%252FDemande.read%2520%252FDemande.write%26client_id%3Dprod_espaceclient%26state%3D0%26redirect_uri%3Dhttps%253A%252F%252Fmonespace.grdf.fr%252F_codexch%26nonce%3D7cV89oGyWnw28DYdI-702Gjy9f5XdIJ_4dKE_hbsvag%26by_pass_okta%3D1%26capp%3Dmeg&realm=%2FexterneGrdf&capp=meg'
-                }
-    
-    resp1 = session.post(LOGIN_BASE_URI, data=payload, headers=headers)
-    logging.debug("1st Auth Response : \n" + resp1.text)
-    if resp1.status_code != requests.codes.ok:
-        print("Login call - error status :"+resp1.status_code+'\n');
-        logging.error("Login call - error status :"+resp1.status_code+'\n')
-        if xlsPath != "":
-            xlsimport()
-        exit()
-    
-    j = json.loads(resp1.text)
-    if j['state'] != "SUCCESS":
-        print("Login call - error status :"+j['state']+'\n');
-        logging.error("Login call - error status :"+j['state']+'\n')
-        if xlsPath != "":
-            xlsimport()
+    payload = SESSION_TOKEN_PAYLOAD.format(userName, password)
+
+    response = session.post(SESSION_TOKEN_URL, data=payload)
+
+    if response.status_code != 200:
+        raise Exception(
+            f"An error occurred while logging in. Status code: {response.status_code} - {response.text} - {response.payload}")
         exit()
 
-    #2nd request
-    headers = {
-                'Referer': 'https://sofa-connexion.grdf.fr:443/openam/oauth2/externeGrdf/authorize?response_type=code&scope=openid profile email infotravaux /v1/accreditation /v1/accreditations /digiconso/v1 /digiconso/v1/consommations new_meg /Demande.read /Demande.write&client_id=prod_espaceclient&state=0&redirect_uri=https://monespace.grdf.fr/_codexch&nonce=7cV89oGyWnw28DYdI-702Gjy9f5XdIJ_4dKE_hbsvag&by_pass_okta=1&capp=meg'
-                }
+    # get auth token
+    session_token = response.json().get("sessionToken")
+    logging.debug("Session token: %s", session_token)
+    jar = http.cookiejar.CookieJar()
+    # self.session = Session()
+    session.headers.update({"Content-Type": "application/json"})
+    session.headers.update({"X-Requested-With": "XMLHttpRequest"})
 
-    resp2 = session.get(API_BASE_URI, allow_redirects=True)
-    logging.debug("2nd API Response : \n" + resp2.text)
-    if resp2.status_code != requests.codes.ok:
-        print("Login 2nd call - error status :"+resp2.status_code+'\n');
-        logging.error("Login 2nd call - error status :"+resp2.status_code+'\n')
+    params = json.loads(AUTH_TOKEN_PARAMS.format(session_token))
+
+    response = session.get(AUTH_TOKEN_URL, params=params, allow_redirects=True, cookies=jar)
+
+    if response.status_code != 200:
+        raise Exception(
+            f"An error occurred while getting the auth token. Status code: {response.status_code} - {response.text}")
         exit()
-    
+
+    auth_token = session.cookies.get("auth_token", domain="monespace.grdf.fr")
+
+    # Create a session.
+    # self.session = Session()
+    session.headers.update({"Host": "monespace.grdf.fr"})
+    session.headers.update({"Domain": "grdf.fr"})
+    session.headers.update({"X-Requested-With": "XMLHttpRequest"})
+    session.headers.update({"Accept": "application/json"})
+    session.cookies.set("auth_token", auth_token, domain="monespace.grdf.fr")
+
     return session
     
 def update_counters(session, start_date, end_date):
